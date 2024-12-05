c8e2e098c588d427de6eed11f028999b4f410263
Avairon <75504999+Avairon@users.noreply.github.com>
2024-12-05 16:51:47 +0700

    Delete Chats_bot_paython directory

--- Chats_bot_paython/not_using/tests.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.insert(1, 'D:\Code\CodePython\chat_bot_python\Chats_bot_paython')
-
-import src.tranzactions as tr
-
-text = tr.read_messages()
-str = "16"
-
-print(text[8] % (str))
